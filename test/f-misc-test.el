5875f546fe9d028bd9d0db940c4792e300b3133d
Johan Andersson <johan.rejeep@gmail.com>
2013-07-10 09:31:09 +0200

    Improve glob tests.

--- test/f-misc-test.el
+++ test/f-misc-test.el
@@ -1,10 +1,19 @@
 (ert-deftest f-glob-test/without-path ()
-  (with-mock
-   (mock (file-expand-wildcards "/default/directory/*.el") :times 1)
-   (with-default-directory
-    (f-glob "*.el"))))
+  (with-sandbox
+   (f-write "foo.el")
+   (f-write "baz.el")
+   (f-mkdir "bar")
+   (f-write "bar/qux.el")
+   (should
+    (equal
+     (mapcar 'f-filename (f-glob "*.el")) '("baz.el" "foo.el")))))
 
 (ert-deftest f-glob-test/with-path ()
-  (with-mock
-   (mock (file-expand-wildcards "path/to/*.el") :times 1)
-   (f-glob "*.el" "path/to")))
+  (with-sandbox
+   (f-write "foo.el")
+   (f-write "baz.el")
+   (f-mkdir "bar")
+   (f-write "bar/qux.el")
+   (should
+    (equal
+     (mapcar 'f-filename (f-glob "*.el" "bar")) '("qux.el")))))
