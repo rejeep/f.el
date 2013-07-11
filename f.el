1e1adf53fb14c6230e8444794cf5ac3ad796a247
Johan Andersson <johan.rejeep@gmail.com>
2013-07-11 08:43:00 +0200

    Add inline doc strings.

--- f.el
+++ f.el
@@ -36,38 +36,50 @@
 (defvar f-path-separator "/")
 
 (defun f-join (&rest args)
+  "Join ARGS to a single path."
   (s-join f-path-separator args))
 
 (defun f-expand (path &optional dir)
+  "Expand PATH relative to DIR (or `default-directory')."
   (expand-file-name path dir))
 
 (defun f-filename (path)
+  "Return the name of PATH."
   (file-name-nondirectory path))
 
 (defalias 'f-parent 'f-dirname)
 (defun f-dirname (path)
+  "Return the parent directory to PATH."
   (let ((parent (file-name-directory (f-expand path default-directory))))
     (if (f-relative? path)
         (f-relative parent)
       parent)))
 
 (defun f-ext (path)
+  "Return the file extension of PATH."
   (file-name-extension path))
 
 (defun f-no-ext (path)
+  "Return everything but the file extension of PATH."
   (file-name-sans-extension path))
 
 (defun f-base (path)
+  "Return the name of PATH, excluding the extension if file."
   (f-no-ext (f-filename path)))
 
 (defun f-relative (path &optional dir)
+  "Return PATH relative to DIR."
   (file-relative-name path dir))
 
 (defun f-write (path &optional content)
+  "Write CONTENT or nothing to PATH. If no content, just create file."
   (with-temp-file path
     (if content (insert content))))
 
 (defun f-mkdir (&rest dirs)
+  "Create directories DIRS.
+
+(f-mkdir \"foo\" \"bar\") will create the directory: \"foo/bar\""
   (let (dir)
     (while dirs
       (setq dir (f-expand (car dirs) dir))
@@ -76,50 +88,69 @@
       (setq dirs (cdr dirs)))))
 
 (defun f-delete (path &optional force)
+  "Delete PATH, which can be file or directory.
+
+If FORCE is t, a directory will be deleted recursively."
   (if (f-file? path)
       (delete-file path nil)
     (delete-directory path force nil)))
 
 (defun f-symlink (source path)
+  "Create a symlink to `source` from `path`."
   (make-symbolic-link source path))
 
 (defun f-move (from to)
+  "Move or rename FROM to TO."
   (rename-file from to t))
 
 (defun f-exists? (path)
+  "Return t if PATH exists, false otherwise."
   (file-exists-p path))
 
 (defalias 'f-dir? 'f-directory?)
 (defun f-directory? (path)
+  "Return t if PATH is directory, false otherwise."
   (file-directory-p path))
 
 (defun f-file? (path)
+  "Return t if PATH is file, false otherwise."
   (file-regular-p path))
 
 (defun f-symlink? (path)
+  "Return t if PATH is symlink, false otherwise."
   (file-symlink-p path))
 
 (defun f-readable? (path)
+  "Return t if PATH is readable, false otherwise."
   (file-readable-p path))
 
 (defun f-writable? (path)
+  "Return t if PATH is writable, false otherwise."
   (file-writable-p path))
 
 (defun f-executable? (path)
+  "Return t if PATH is executable, false otherwise."
   (file-executable-p path))
 
 (defun f-absolute? (path)
+  "Return t if PATH is absolute, false otherwise."
   (file-name-absolute-p path))
 
 (defun f-relative? (path)
+  "Return t if PATH is relative, false otherwise."
   (not (f-absolute? path)))
 
 (defun f-size (path)
+  "Return size of PATH.
+
+If PATH is a file, return size of that file. If PATH is
+directory, return sum of all files in PATH."
   (if (f-directory? path)
       (apply '+ (-map 'f-size (f-files path nil t)))
     (nth 7 (file-attributes path))))
 
 (defun f-read (path)
+  "Return content of PATH."
   (with-temp-buffer
     (insert-file-contents-literally path)
     (buffer-substring-no-properties
@@ -127,6 +158,7 @@
      (point-max))))
 
 (defun f-glob (pattern &optional path)
+  "Find PATTERN in PATH."
   (file-expand-wildcards
    (f-join (or path default-directory) pattern)))
 
@@ -152,14 +184,21 @@
     result))
 
 (defun f-entries (path &optional fn recursive)
+  "Find all files and directories in PATH.
+
+FN - called for each found file and directory. If FN returns a thruthy
+value, file or directory will be included.
+RECURSIVE - Search for files and directories recursive."
   (let ((entries (f--entries path recursive)))
     (if fn (-select fn entries) entries)))
 
 (defun f-directories (path &optional fn recursive)
+  "Find all directories in PATH. See `f-files`."
   (let ((directories (-select 'f-directory? (f--entries path recursive))))
     (if fn (-select fn directories) directories)))
 
 (defun f-files (path &optional fn recursive)
+  "Find all files in PATH. See `f-files`."
   (let ((files (-select 'f-file? (f--entries path recursive))))
     (if fn (-select fn files) files)))
 
