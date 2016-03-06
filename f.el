fe35efd47796c269af5622b8f2f2fd0b130b6470
Johan Andersson <johan.rejeep@gmail.com>
2016-03-06 13:47:35 +0100

    v1.18.1

--- f.el
+++ f.el
@@ -4,7 +4,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.17.3
+;; Version: 0.18.1
 ;; Keywords: files, directories
 ;; URL: http://github.com/rejeep/f.el
 ;; Package-Requires: ((s "1.7.0") (dash "2.2.0"))
