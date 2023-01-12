3aee89b079f55ed56b531c8c48729f87da496119
Lucien Cartier-Tilet <lucien@phundrak.com>
2023-01-12 12:20:01 +0100

    Merge back some tests
    These tests were in separate functions when debugging, I forgot to
    merge them back

--- test/f-stats-test.el
+++ test/f-stats-test.el
@@ -60,16 +60,8 @@
 (ert-deftest f-change-time/return-time-for-file ()
   (with-playground
     (f-touch "foo.txt")
-    (should (equal (length (f-change-time "foo.txt")) 4))))
-
-(ert-deftest f-change-time/return-time-for-file--timestamp ()
-  (with-playground
-    (f-touch "foo.txt")
-    (should (consp (f-change-time "foo.txt" t)))))
-
-(ert-deftest f-change-time/return-time-for-file--seconds ()
-  (with-playground
-    (f-touch "foo.txt")
+    (should (equal (length (f-change-time "foo.txt")) 4))
+    (should (consp (f-change-time "foo.txt" t)))
     (should (integerp (f-change-time "foo.txt" 'seconds)))))
 
 ;;;; f-modification-time
